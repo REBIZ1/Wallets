65e3ecd6b33f689f923030d98da6ad57cd75f87c
Arthur <arthur155188@gmail.com>
2026-05-27 19:41:18 +0500

    Добавлен менеджер работы с базой данных, который создает сессии и предоставляет доступ к репозиториям

---
+++ src/utils/db_manager.py
@@ -0,0 +1,27 @@
+from src.repositories.wallets import WalletRepository
+
+
+class DBManager:
+    """
+    Менеджер работы с базой данных
+    создает сессии и предоставляет доступ к репозиториям
+    """
+
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        self.wallets = WalletRepository(self.session)
+        return self
+
+    async def __aexit__(self, *args):
+        await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
