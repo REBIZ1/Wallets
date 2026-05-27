e70e2316bb8b12e74ea309017b78cc995f782e8f
Arthur <arthur155188@gmail.com>
2026-05-27 19:30:05 +0500

    Настроенно подключение к бд

---
+++ src/core/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from src.core.config import settings
+
+db_params = {}
+if settings.MODE == "TEST":
+    db_params = {"poolclass": NullPool}
+engine = create_async_engine(settings.DB_URL, **db_params)
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
